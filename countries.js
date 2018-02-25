689e001f69caecf33ef67618f9de3a0d5b73cf03
Sethupathi Sivasamy <ssethupathi@temenos.com>
2018-02-25 13:13:14 +0530

    initial version working with cities on all countries

--- countries.js
+++ countries.js
@@ -11,33 +11,49 @@ function Country() {
     this.cities = [];
 };
 
+function City() {
+  this.name;
+  this.code;
+  this.state;
+  this.location;
+}
+
 var read = function(callback) {
 
-  var stream = fs.createReadStream("./countries0.csv");
+  var stream = fs.createReadStream("./countries.csv");
   let c = new Country();
+  let totCountries = 0;
   var csvStream = csv()
     .on("data", function(data) {
-        logger.log.debug(data);
+        if (data[0] == '') {
          if (data[2] == '') {
-           logger.log.debug('Init country ',data[2]);
-           if (!c.name) {
+           let name = data[3].substring(1);
+           logger.log.debug('Init country', name);
+           if (c.name) {
+             totCountries++;
              callback(c);
              c = new Country();
            }
-           c.name = data[3];
+           c.name = name;
            c.code = data[1];
          } else {
-           c.cities.push(data[3]);
+           c.cities.push(JSON.stringify(buildCity(data)));
          }
-    })
+       }
+     })
     .on("end", function(){
-         console.log("done");
+         logger.log.debug("Loading of ",totCountries, "complete");
   });
   stream.pipe(csvStream);
 }
 
-var parse = function(rawData) {
-
+var buildCity = function(data) {
+  let city = new City();
+  city.name = data[3];
+  city.code = data[2];
+  city.state = data[5];
+  city.location = data[10];
+  return city;
 }
 
 module.exports = {
--- data-solr.js
+++ data-solr.js
@@ -9,46 +9,89 @@ let client = solr.createClient('127.0.0.1', 8983, 'c0', '/solr');
 var load = function() {
   logger.log.debug('Solr load called');
   countries.read(function(c) {
-    logger.log.debug('Country ',JSON.stringify(c));
+    logger.log.debug('Adding data for ', c.name);
+    client.add(c, function(err, obj) {
+      if (err) {
+        logger.log.debug('Failed to add ',c);
+        logger.log.debug('Error ', err);
+      } else {
+        logger.log.debug('Country successfully added to solr ', c.name);
+        client.softCommit();
+      }
+    });
   });
-  logger.log.debug('Read completed');
-
-  // let data = {
-  //   name: 'India',
-  //   code: 'IN',
-  //   cities: ['Ahmedabad', 'Bangalore', 'Chennai', 'Delhi']
-  // }
-  // logger.log.debug('Adding data ', data);
-  //
-  // client.add(data, function(err, obj) {
-  //   if (err) {
-  //     logger.log.debug('Error1:', err);
-  //   } else {
-  //     logger.log.debug('Obj1:', obj);
-  //     client.softCommit();
-  //   }
-  // });
 }
 
 var search = function(chars, callback) {
-  logger.log.debug('Solr search called');
-  chars = '*' + chars + '*';
-  client.search('q=name:'+chars, function(err, obj) {
+  let query = encodeURI(buildQuery(chars));
+  client.search(query, function(err, obj) {
     let matched = [];
     if (err) {
-      logger.log.debug('Error2:', err);
+      logger.log.debug('Search failed', err);
     } else {
-      logger.log.debug('Obj2:', );
+      let docs = obj.response.docs;
+      if (docs.length > 0) {
+        matched = matcher(chars, docs);
+      }
     }
-    let docs = obj.response.docs;
-    logger.log.debug('Response', docs[0]);
+    callback(err, matched);
+  })
+}
+
+var matcher = function(chars, docs) {
+  let slashExist = isSlashExist(chars);
+  let parent = extractParent(chars);
+  let child = extractChild(chars);
+  let matched = [];
+  if (!slashExist) {
     for (var idx = 0; idx < docs.length; idx++) {
       matched.push(docs[idx].name[0]);
     }
-    callback(err, matched);
-  }).setTimeout(200, function() {
-    logger.log.debug('search timeout');
-  });
+  } else {
+    let cities = JSON.parse('[' + docs[0].cities + ']');
+    for (var idx = 0; idx < cities.length; idx++) {
+      let city = cities[idx];
+      var regex = new RegExp(child, 'i');
+      if (city.name.match(regex) || (!child && slashExist)) {
+        let pushed  = city.name + ' State:' + city.state + ' Location:'+city.location;
+        matched.push(pushed);
+      }
+    }
+  }
+  return matched;
+}
+
+var buildQuery = function(chars) {
+  let slashExist = isSlashExist(chars);
+  let parent = extractParent(chars);
+  let child = extractChild(chars);
+  if (slashExist) {
+      return 'q=name:' + parent + ' AND cities:*' + child + '*';
+    } else {
+      return 'q=name:*' + parent + '*';
+    }
+}
+
+var extractChild = function(chars) {
+  let slashIdx = chars.indexOf("/");
+  if (slashIdx > 0 && chars.length > slashIdx + 1) {
+      return chars.substring(slashIdx + 1);
+  } else {
+    return ''
+  }
+}
+
+var extractParent = function(chars) {
+  let slashIdx = chars.indexOf("/");
+  if (slashIdx > 0) {
+    return chars.substring(0, slashIdx);
+  } else {
+    return chars;
+  }
+}
+
+var isSlashExist = function(chars) {
+  return chars.indexOf("/") > 0;
 }
 
 module.exports = {
